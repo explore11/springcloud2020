2bb163f1b4c9b686a101b270f74779d2f38686dc
explore11 <prefect_start@163.com>
2020-03-24 21:50:03 +0800

    gateway路由的两种配置以及动态路由的配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.atguigu.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route1", r -> r.path("/guoji").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+    @Bean
+    public RouteLocator customRouteLocator_junshi(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route2", r -> r.path("/mil").uri("http://news.baidu.com/mil")).build();
+        return routes.build();
+    }
+}
