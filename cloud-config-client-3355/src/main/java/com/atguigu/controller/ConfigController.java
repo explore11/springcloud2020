7d6db7c4ff8a5eff3d1303e772a7a504f2698ed7
explore11 <prefect_start@163.com>
2020-03-29 01:11:52 +0800

    增加config配置中心的客户端 并简单的测试

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/controller/ConfigController.java
@@ -0,0 +1,16 @@
+package com.atguigu.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+    @Value("${s1pring.application.name}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
