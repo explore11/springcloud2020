eae01af857c75f10d26e1536cd80f7f5db5bb77d
explore11 <prefect_start@163.com>
2020-03-29 15:49:43 +0800

    实现 服务配置文件的动态刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/controller/ConfigController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/controller/ConfigController.java
@@ -1,16 +1,18 @@
 package com.atguigu.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope    //动态刷新配置  添加对外暴露的接口 添加注解后 需要post请求一下  curl -X POST "http://localhost:3355/actuator/refresh"
 public class ConfigController {
     @Value("${s1pring.application.name}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo(){
+    public String getConfigInfo() {
         return configInfo;
     }
 }
